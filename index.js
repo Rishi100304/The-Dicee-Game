de83b327af61a6f962f78bb3f56eedb28d63f841
Rishi Anand <143389614+Rishi100304@users.noreply.github.com>
2024-07-23 20:46:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice"+ randomNumber1 +".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice"+ randomNumber2 +".png");
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector(".refresh").innerHTML = "🚩Player 1 Wins!"
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector(".refresh").innerHTML = "Player 2 Wins!🚩"
+} else {
+    document.querySelector(".refresh").innerHTML = "Draw!"
+}
+    
